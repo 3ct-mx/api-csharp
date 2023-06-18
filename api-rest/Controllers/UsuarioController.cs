a0add9a9ea4b19e700264be5005a8cdb139cccfa
Jorge Sanchez <jorgesanchez@Jorges-MacBook-Pro.local>
2023-06-18 12:18:03 -0700

    Primera version del endpoint

---
+++ api-rest/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using api_rest.Model;
+
+namespace api_rest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController
+	{
+
+        // Lista vacia
+        public List<Usuario> lista = new List<Usuario>();
+
+        public UsuarioController()
+		{
+            lista.Add(new Usuario("JSB123", "correo@3ct.mx", "SDFsdsdf@##$DDC"));
+            lista.Add(new Usuario("RocioABC", "rocio@gmail.com", "SDFsddsfsdf$DDC"));
+
+        }
+
+        [HttpGet(Name = "GetUsuario")]
+        public List<Usuario> dameLista()
+        {
+            return lista;
+        }
+	
+
+	}
+}
+
---
+++ api-rest/Model/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace api_rest.Model
+{
+	public class Usuario
+	{
+
+		public string id { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+
+        public Usuario()
+		{
+		}
+
+        public Usuario(string id, string email, string password)
+        {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+        }
+    }
+}
+
